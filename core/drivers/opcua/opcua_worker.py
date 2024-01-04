8370fe521075f2f0b6b50a6b1d24a5bed268e7f1
trungnguyenduy <trung.nguyen@piscada.com>
2024-01-04 22:07:23 +0700

    add opcua client certificate

--- core/drivers/opcua/opcua_worker.py
+++ core/drivers/opcua/opcua_worker.py
@@ -1,13 +1,21 @@
 import asyncio
 import logging
+import os
+import socket
 import time
 from datetime import datetime
+from pathlib import Path
 from typing import Any
 
 from asyncua import Client, ua, Node
 from asyncua.common.subscription import Subscription, DataChangeNotif
+from asyncua.crypto.cert_gen import setup_self_signed_certificate
+from asyncua.crypto.security_policies import SecurityPolicyBasic256Sha256
 from asyncua.ua import String, Int16, Int32
+from cryptography.hazmat._oid import ExtendedKeyUsageOID
 
+from aquacloud_common.utilities.cetification_utils import cert_gen
+from constants import CONFIG_PATH
 from core.drivers.opcua.server_model import ServerModel
 from core.opcua.opcua_server import OPCUAServer
 from models.mapping_model import MappingModel
@@ -38,6 +46,7 @@ class OpcuaWorker:
         self._subscriber: Subscription = None
 
     async def subscribe(self):
+        self._subscription_nodes.clear()
         for sensor in self._sensors:
             try:
                 for key in sensor.mapping:
@@ -46,16 +55,38 @@ class OpcuaWorker:
                     node = self._client.get_node(node_id)
                     if node is not None:
                         self._subscription_nodes.append(node)
-                await self._subscriber.subscribe_data_change(self._subscription_nodes)
             except Exception as e:
                 _logger.warning(e)
+        await self._subscriber.subscribe_data_change(self._subscription_nodes)
 
     async def run(self):
         while True:
+            cert = Path(f"peer-certificate.der")
+            private_key = Path(f"peer-private-key.pem")
+            host_name = socket.gethostname()
+            client_app_uri = f"urn:{host_name}:aquacloud:myselfsignedclient"
+            await setup_self_signed_certificate(private_key,
+                                                cert,
+                                                client_app_uri,
+                                                host_name,
+                                                [ExtendedKeyUsageOID.CLIENT_AUTH],
+                                                {
+                                                    'countryName': 'NO',
+                                                    'stateOrProvinceName': 'N/A',
+                                                    'localityName': 'N/A',
+                                                    'organizationName': "AquaCloud",
+                                                })
+
             try:
                 self._client = Client(self._server.endpoint)
                 self._client.set_user(self._server.username)
                 self._client.set_password(self._server.password)
+                await self._client.set_security(
+                    policy=SecurityPolicyBasic256Sha256,
+                    certificate=str(cert),
+                    private_key=str(private_key),
+                    mode=ua.MessageSecurityMode.SignAndEncrypt
+                )
                 async with self._client:
                     self._subscriber = await self._client.create_subscription(500, self)
                     try:
@@ -69,9 +100,10 @@ class OpcuaWorker:
             await asyncio.sleep(DISCOVERY_INTERVAL)
 
     async def _handle_data_change(self, node: Node, value: Any):
-        ns = node.nodeid.NamespaceIndex
+        ns = self._opc_server.get_namespace()
+        remote_ns = node.nodeid.NamespaceIndex
         i = node.nodeid.Identifier
-        key = self._unit_id + ":" + str(ns) + "_" + str(i)
+        key = self._unit_id + ":" + str(remote_ns) + "_" + str(i)
         for mapping_key in self._mapping:
             if key in mapping_key:
                 mapping = self._mapping[mapping_key]
